f2f2d127f06a52bc185df9b2637641d80e7dde39
ViciousLabs <theviciouslabs@gmail.com>
2023-03-17 11:11:21 +0100

    Updated deployment steps

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,11 @@ cp CNAME ./public
 
 # deploy
 cd public
+git stash
+git fetch
+git checkout gh-pages
+git reset --hard origin/gh-pages
+git stash apply
 git add *
 git commit -m "Sync changes"
 git push origin gh-pages
